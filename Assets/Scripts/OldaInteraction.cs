263473a17b83a25a6fab2da69318f2952720fba7
seankao <sean1201@gmail.com>
2022-06-19 16:20:02 +0800

    Add 15 vs Obear timeline.

---
+++ Assets/Scripts/OldaInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OldaInteraction : Interactable
+{
+    [SerializeField]
+    private DialogSystem _dialogSystem;
+
+    protected override void Interaction(Player player)
+    {
+        StartCoroutine(Play());
+    }
+
+    IEnumerator Play()
+    {
+        yield return _dialogSystem.Play();
+        SceneManager.LoadSceneAsync("Level2", LoadSceneMode.Single);
+    }
+}
