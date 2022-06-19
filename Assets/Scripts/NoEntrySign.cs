c2105fe2c3d96d6e70a5f2d0fbfe0116032b6cbf
Tommy881003 <tommy031088@gmail.com>
2022-06-19 19:42:48 +0800

    asdasdasdasd

---
+++ Assets/Scripts/NoEntrySign.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoEntrySign : MonoBehaviour
+{
+    [SerializeField]
+    private Image sign;
+    [SerializeField]
+    private float clearTime = 0.3f;
+    private float clearTimer;
+
+    private void Start() 
+    {
+        clearTimer = 0;    
+    }
+
+    public void Show() { clearTimer = clearTime; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sign.color = new Color(1,1,1, clearTimer / clearTime);
+        clearTimer = Mathf.Clamp(clearTimer - Time.deltaTime, 0, clearTime);
+    }
+}
