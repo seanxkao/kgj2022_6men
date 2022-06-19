5de28c19a79dca9eb57a67a3e921bb041b940ce0
seankao <sean1201@gmail.com>
2022-06-19 13:20:37 +0800

    Little change.

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -2,36 +2,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Playables;
-using UnityEngine.Timeline;
+using UnityEngine.Events;
 
 public class DialogSystem : MonoBehaviour
 {
-    [Serializable]
-    public class ScenarioCommand
+    [SerializeField]
+    private CanvasGroup _mainPanel;
+    [SerializeField]
+    private Timeline[] _timelines;
+    [SerializeField]
+    private Button _playButton;
+    [SerializeField]
+    private CanvasGroup _overPanel;
+    [SerializeField]
+    private Button _over;
+    [SerializeField]
+    private Text _textView;
+    [SerializeField]
+    private float _playSpeed;
+    
+    private void Awake()
     {
-        [Serializable]
-        public class Next
+        _playButton.onClick.AddListener(() => 
         {
-            public string Condition;
-            public string Id;
-        }
-
-        public string Id;
-        public PlayableDirector Playable;
-        public Next[] Nexts;
+            StartCoroutine(Play(_timelines));
+        });
     }
 
-    // This system
-    [Serializable]
-    public class OptionCommand
+    public IEnumerator Play(Timeline[] timelines)
     {
-        public class Option
+        foreach(var timeline in timelines)
         {
-            public string Text;
-            public string Eval;
+            yield return _PlayScenario(timeline);
+        }
+    }
+
+    private IEnumerator _PlayScenario(Timeline timeline)
+    {
+        bool isFinished = false;
+        Action<DialogEvent> onDialog = dialog =>
+        {
+            timeline.Playable.playableGraph.GetRootPlayable(0).SetSpeed(0f);
+            StartCoroutine(_PlayDialog(dialog.Text));
+        };
+
+        UnityAction onClick = new UnityAction(() =>
+        {
+            _overPanel.blocksRaycasts = false;
+            _overPanel.interactable = false;
+            timeline.Playable.playableGraph.GetRootPlayable(0).SetSpeed(1f);
+        });
+
+        Action<PlayableDirector> onFinished = _ => isFinished = true;
+
+        timeline.Playable.stopped += onFinished;
+        timeline.OnDialog += onDialog;
+        _over.onClick.AddListener(onClick);
+
+        timeline.Playable.Play();
+        while (!isFinished)
+        {
+            yield return null;
         }
 
-        public Option[] Options;
+        timeline.Playable.stopped -= onFinished;
+        timeline.OnDialog -= onDialog;
+        _over.onClick.RemoveListener(onClick);
+    }
+
+    private IEnumerator _PlayDialog(string text)
+    {
+        yield return _PlayText(text);
+        _textView.text = text;
+        _overPanel.blocksRaycasts = true;
+        _overPanel.interactable = true;
+    }
+
+    private IEnumerator _PlayText(string text)
+    {
+        var textPlayer = new TextPlayer(text);
+        float length = textPlayer.Length;
+        yield return Interpolation.Play(0f, length, length / _playSpeed, t =>
+        {
+            _textView.text = textPlayer.GetText(Mathf.FloorToInt(t));
+        });
     }
 }
