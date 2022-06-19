b750d1553c629fc3a47f696cf17a7e249177396c
Tommy881003 <tommy031088@gmail.com>
2022-06-19 17:34:57 +0800

    Add bgm

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    private static BGM instance = null;
+    private void Awake() 
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);    
+        }
+        else
+            Destroy(this);
+    }
+}
