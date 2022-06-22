289f6ab7e4e4058d4a9ec98a1252acdf72173712
Jamie Tu <jd3main@gmail.com>
2022-06-23 03:33:59 +0800

    Add reload level function

---
+++ Assets/Scripts/ReloadLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadLevel : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ReloadScene());
+        }
+    }
+
+    IEnumerator ReloadScene()
+    {
+        const float t = 0.5f;
+        const float dt = 0.02f;
+        for (int i = 0; i < t/dt; i++)
+        {
+            Camera.main.orthographicSize *= 2;
+            yield return new WaitForSecondsRealtime(dt);
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
